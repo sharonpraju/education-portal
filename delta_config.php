ff4c244e87bf4c27aac342949c75786c4256495d
Tibinsunny <tibinsunny95@gmail.com>
2020-07-25 11:08:12 -0700

    added new database

--- delta_config.php
+++ delta_config.php
@@ -2,10 +2,10 @@
 //Database Connections Here
 function OpenCon()
  {
- $dbhost = "sql12.freemysqlhosting.net";
- $dbuser = "sql12356824";
- $dbpass = "JZESgt568N";
- $db = "sql12356824";
+ $dbhost = "164.68.110.46:3306";
+ $dbuser = "delta_x";
+ $dbpass = 'x3v$ANKgwm52gbjz';
+ $db = "delta_db";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
