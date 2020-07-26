56b58d86a203b73c6c6172ca52e5d02bf8525e66
athulbabykurian <athulofficial01@gmail.com>
2020-07-26 21:19:04 +0530

    Revert "Merge branch 'test' of https://github.com/sharonpraju/education-portal into test"
    This reverts commit 964f356ddb1393bf26cf1fc9bd3fe7ea852cff98.

--- admin/tables/table_action.php
+++ admin/tables/table_action.php
@@ -2,61 +2,14 @@
 //action.php
 $connect = mysqli_connect('164.68.110.46:3306', 'delta_x', 'x3v$ANKgwm52gbjz', 'delta_db');
 $input = filter_input_array(INPUT_POST);
-$newArray = array_values($input);
 
-echo($newArray[1]);
-if($newArray[1]=='delete')
-{
-    $delete_id=$newArray[0];
-    $query = "
- DELETE FROM users 
- WHERE id = '".$delete_id."'
- ";
- mysqli_query($connect, $query);
- echo ("Deleted");
-}
-else
-{
-    $name=$newArray[1];
-    $who=$newArray[2];
-    $department=$newArray[3];
-    $email=$newArray[4];
-    $ban_status=$newArray[5];
-    $query = "
-    UPDATE users 
-    SET 
-    name = '".$name."', 
-    who = '".$who."'
-    department = '".$department."'
-    email = '".$email."'
-    ban_status = '".$ban_status."' 
-    WHERE id = '".$newArray[0]."'
-    ";
-    echo($query);
-   
-    echo(mysqli_query($connect, $query));
-    
-
-
-}
-
-
-
-
-
-
-
-
-
-
-/*
-$name=$newArray[0];
-$who = $newArray[1];
+$name = mysqli_real_escape_string($connect, $input["name"]);
+$who = mysqli_real_escape_string($connect, $input["who"]);
 $department = mysqli_real_escape_string($connect, $input["department"]);
 $email = mysqli_real_escape_string($connect, $input["email"]);
-$ban_status = mysqli_real_escape_string($connect, $input["ban_status"]);*/
+$ban_status = mysqli_real_escape_string($connect, $input["ban_status"]);
 
-/*
+
 
 if($input["action"] === 'edit')
 {
@@ -75,7 +28,7 @@ if($input["action"] === 'edit')
  mysqli_query($connect, $query);
 
 }
-if($newArray[6] === 'delete')
+if($input["action"] === 'delete')
 {
  $query = "
  DELETE FROM users 
@@ -84,6 +37,6 @@ if($newArray[6] === 'delete')
  mysqli_query($connect, $query);
 }
 
-echo json_encode($input);*/
+echo json_encode($input);
 
 ?>
