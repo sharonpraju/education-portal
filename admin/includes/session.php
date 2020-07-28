cc583e6a245fd4b73d89df0a86a779b9e2a6f9d4
athulbabykurian <athulofficial01@gmail.com>
2020-07-28 15:10:39 +0530

    sharon code nooku

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,11 +2,8 @@
 session_start();
 if(!isset( $_SESSION['admin']))
 {
-<<<<<<< HEAD
-    echo('<script>alert("Your session has expired")<?script>');
-=======
->>>>>>> e392f7cd61fc2b629cb62b512c910f41da56aa78
-    header('Location: index.php');
+    echo('<script>alert("Your session has expired")</script>');
+    header('Location: ../index.php');
     exit;
     // kalla panni erangi poda
 }
