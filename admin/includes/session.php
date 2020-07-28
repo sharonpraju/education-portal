2e43179cf24f10a4d482ca48d67133f1e49b7488
sharonpraju <47088116+sharonpraju@users.noreply.github.com>
2020-07-28 15:26:57 +0530

    Update session.php

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -3,7 +3,7 @@ session_start();
 if(!isset( $_SESSION['admin']))
 {
     echo('<script>alert("Your session has expired")</script>');
-    header('Location: ../index.php');
+    header('Location: ./index.php');
     exit;
     // kalla panni erangi poda
 }
